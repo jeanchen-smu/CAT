f291b6b13bd433bf8cb1ca2a209742f8dc17db15
hbtruong2017 <hbtruong.2017@sis.smu.edu.sg>
2020-02-04 09:24:48 +0800

    Create run script for backend

---
+++ backend/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pip install Flask
+pip install flask_jwt_extended
+pip install pandas
+pip install nltk
+env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install mysqlclient
+pip install gensim
+pip install sklearn
+pip install telegram
+pip install uritools
+pip install regex
+python api.py
