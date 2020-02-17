0d5abb0130d5a2a8304e6c1540af7dd3a92cb971
haibang_truong <haibang97@yahoo.com>
2020-02-17 11:33:06 +0800

    Update start script to install mysql client for windows

--- backend/start.sh
+++ backend/start.sh
@@ -3,7 +3,8 @@ pip install Flask
 pip install flask_jwt_extended
 pip install pandas
 pip install nltk
-env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install mysqlclient
+# env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install mysqlclient
+pip install --only-binary :all: mysqlclient
 pip install gensim
 pip install sklearn
 pip install telegram
